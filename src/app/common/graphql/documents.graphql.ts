c6a4f48ee533e4cd6a07c6a0f8c0a94db0d39a2f
niaiko <niaikorakoto@yahoo.com>
2021-10-15 14:46:22 +0300

    first commit

---
+++ src/app/common/graphql/documents.graphql.ts
@@ -0,0 +1,91 @@
+import {gql} from 'apollo-angular';
+
+
+import { ADDRESS_FRAGMENT, ASSET_FRAGMENT, COUNTRY_FRAGMENT } from './fragments.graphql';
+
+export const GET_CUSTOMER_ADDRESSES = gql`
+    query GetCustomerAddresses {
+        activeCustomer {
+            id
+            addresses {
+                ...Address
+            }
+        }
+    }
+    ${ADDRESS_FRAGMENT}
+`;
+
+export const GET_AVAILABLE_COUNTRIES = gql`
+    query GetAvailableCountries {
+        availableCountries {
+            ...Country
+        }
+    }
+    ${COUNTRY_FRAGMENT}
+`;
+
+export const GET_ACTIVE_CUSTOMER = gql`
+    query GetActiveCustomer {
+        activeCustomer {
+            id
+            firstName
+            lastName
+            emailAddress
+            phoneNumber
+        }
+    }
+`;
+export const GET_COLLECTIONS = gql`
+    query GetCollections($options: CollectionListOptions) {
+        collections(options: $options) {
+            items {
+                id
+                name
+                slug
+                parent {
+                    id
+                    name
+                    slug
+                }
+                featuredAsset {
+                    ...Asset
+                }
+            }
+        }
+    }
+    ${ASSET_FRAGMENT}
+`;
+
+
+export const GET_MENU_PARENT = gql`
+query {
+    menus
+  }
+`;
+
+export const GET_COLLECTION_PARENT = gql`
+query collectionByParent($id: ID!) {
+    collectionByParrent(id: $id)
+  }
+`;
+
+export const GET_PRODUIT_MENU = gql`
+  query productMenu($id: ID!) {
+    productMenu(id: $id) {
+      enabled
+      id
+      name
+      description
+      customFields{productRecommendationsEnabled, option, allergenes}
+      assets {
+        source
+      }
+      variants {
+        id
+        priceWithTax
+        price
+        currencyCode
+      }
+    }
+  }
+`;
---
+++ src/app/core/components/product-list/product-list.graphql.ts
@@ -0,0 +1,113 @@
+import {gql} from 'apollo-angular';
+
+
+import { ASSET_FRAGMENT } from '../../../common/graphql/fragments.graphql';
+
+export const SEARCH_PRODUCTS = gql`
+    query SearchProducts($input: SearchInput!) {
+        search(input: $input) {
+            items {
+                productId
+                slug
+                productName
+                description
+                priceWithTax {
+                    ... on PriceRange {
+                        min
+                        max
+                    }
+                }
+                productAsset {
+                    id
+                    preview
+                    focalPoint {
+                        x
+                        y
+                    }
+                }
+            }
+            totalItems
+            facetValues {
+                count
+                facetValue {
+                    id
+                    name
+                    facet {
+                        id
+                        name
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const GET_COLLECTION = gql`
+    query GetCollection($id: ID, $slug: String) {
+        collection(id: $id, slug: $slug) {
+            id
+            name
+            slug
+            description
+            featuredAsset {
+                ...Asset
+            }
+            breadcrumbs {
+                id
+                slug
+                name
+            }
+            children {
+                id
+                slug
+                featuredAsset {
+                    ...Asset
+                }
+                name
+            }
+        }
+    }
+    ${ASSET_FRAGMENT}
+`;
+
+export const GET_PRODUCT_MENU = gql`
+    query GetProduct($id: ID) {
+        collection(id: $id) {
+            id
+            name
+            slug
+            description
+            featuredAsset {
+                ...Asset
+            }
+        }
+    }
+    ${ASSET_FRAGMENT}
+`;
+
+export const GET_PRODUCT_BY_MENU = gql`
+        query product($id: ID) {
+            productMenu(id: $id) {
+            id
+            name
+            slug
+            description
+            variants {
+                productId
+                price
+                priceWithTax
+              }
+            featuredAsset {
+                id
+                width
+                height
+                name
+                preview
+                focalPoint {
+                x
+                y
+                }
+            }
+            }
+        }
+`;
